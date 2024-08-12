35f3e3b2cc3943e3a029410a1c365fb92cd4222d
PrSalvator <kirill.leono2016yandex.ru@gmail.com>
2024-08-12 14:55:51 +0600

    get first response

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import { routes } from "./src/routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api", routes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
